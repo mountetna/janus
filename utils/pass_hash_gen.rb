7401e95aea625ccb431ec6d0897cc6f0728c82cd
Jason Cater <jason.cater@ucsf.edu>
2016-11-10 17:39:28 -0800

    Adding a utility to hash passwords.

---
+++ utils/pass_hash_gen.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/ruby
+
+require 'digest'
+require '../server/conf'
+require '../server/service/sign_service'
+
+def print_help
+  
+  puts ''
+  puts ' desc: A simple util to generate a passhash for the Janus Auth server.'
+  puts 'usage: ./pass_hash_gen.rb [plain text password]'
+  puts ''
+end
+
+# If the argument is 'nil', or if there are to many arguments, or if the
+# argument is an empty string, then bail.
+if ARGV[0].nil? || ARGV.length != 1 || ARGV[0].to_s.length == 0
+
+  print_help
+  exit 1
+end
+
+puts SignService::hash_password([ARGV[0].to_s, Conf::PASS_SALT], Conf::PASS_ALGO)
