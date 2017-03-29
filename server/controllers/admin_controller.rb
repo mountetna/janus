e557146e683ac0e2187c142d03721afa884d065d
Jason Cater <jason.cater@ucsf.edu>
2017-03-29 13:52:24 -0700

    Changing the user/project selection to use 'email' and 'project_name'.

--- server/controllers/admin_controller.rb
+++ server/controllers/admin_controller.rb
@@ -105,8 +105,8 @@ class AdminController < BasicController
   def save_perm(perm)
 
     # Check if the user and project are existant.
-    user = Models::User[:id=> perm['user_email']]
-    pjkt = Models::Project[:id=> perm['project_name']]
+    user = Models::User[:email=> perm['user_email']]
+    pjkt = Models::Project[:project_name=> perm['project_name']]
     if !user || !pjkt then return false end
 
     # Check if there is currently a permission with the user and project.
