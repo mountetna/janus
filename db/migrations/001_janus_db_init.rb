314cf35725afc4950b52f365253a7e1533844b26
Jason Cater <jason.cater@ucsf.edu>
2017-05-25 11:31:37 -0700

    Adding 'project_full_name'. General syntax cleanup.

--- db/migrations/001_janus_db_init.rb
+++ db/migrations/001_janus_db_init.rb
@@ -1,69 +1,58 @@
 Sequel.migration do 
-
   up do
-
     extension(:constraint_validations)
     create_constraint_validations_table
 
     create_table(:groups) do
-
-      primary_key :id
-      String :group_name, :null=>false
+      primary_key(:id)
+      String(:group_name, {null: false})
     end
 
     create_table(:projects) do
-
-      primary_key :id
-      foreign_key :group_id, :groups
-      String :project_name, :null=>false, :unique=>true
-      String :project_name_full, :null=>false, :unique=>true
+      primary_key(:id)
+      foreign_key(:group_id, :groups)
+      String(:project_name, {null: false, unique: true})
+      String(:project_name_full, {null: false, unique: true})
     end
 
     create_table(:users) do
-
-      primary_key :id
-      String :email, :null=>false, :unique=>true
-      String :first_name
-      String :last_name
-      String :pass_hash
-      DateTime :user_create_stamp, :null=>false, :default=>Time.now
+      primary_key(:id)
+      String(:email, {null: false, unique: true})
+      String(:first_name)
+      String(:last_name)
+      String(:pass_hash)
+      DateTime(:user_create_stamp, {null: false, default: Time.now})
     end
 
     create_table(:apps) do
-
-      primary_key :id
-      String :app_key, :null=> false
-      String :app_name, :null=> false
+      primary_key(:id)
+      String(:app_key, {null: false})
+      String :app_name, {null: false})
     end
 
     create_table(:permissions) do
+      primary_key(:id)
+      foreign_key(:user_id, :users)
+      foreign_key(:project_id, :projects)
 
-      primary_key :id
-      foreign_key :user_id, :users
-      foreign_key :project_id, :projects
-
-      String :role, :null=>false
+      String(:role, {null: false})
       validate do
-        
-        includes ['administrator', 'editor', 'viewer'], :role
+        includes(['administrator', 'editor', 'viewer'], :role)
       end
     end
 
     create_table(:tokens) do
-
-      primary_key :id
-      String :token, :null=>false
-      foreign_key :user_id, :users
-      DateTime :token_login_stamp, :null=>false, :default=>Time.now
-      DateTime :token_expire_stamp, :null=>false
-      DateTime :token_logout_stamp, :null=>false
+      primary_key(:id)
+      String(:token, null: false)
+      foreign_key(:user_id, :users)
+      DateTime(:token_login_stamp, {null: false, default: Time.now})
+      DateTime(:token_expire_stamp,{null: false})
+      DateTime(:token_logout_stamp,{null: false})
     end
   end
 
   down do
-
     extension(:constraint_validations)
-
     drop_table(:tokens)
     drop_table(:permissions)
     drop_table(:apps)
