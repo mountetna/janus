04d9420ecd25e51ab377c5855edfe4f9582c74f9
Jason Cater <jason.cater@ucsf.edu>
2017-09-06 16:29:43 -0700

    Chaning the login endpoints. Needed for proper shibboleth operation.

--- lib/server.rb
+++ lib/server.rb
@@ -4,14 +4,14 @@ class Janus
 
     # Only one of these two end points gets used. If you are using Shibboleth
     # then enable the appropriate end point.
-    get '/', 'user_log#login'
-    #get '/', 'user_log#login_shib'
+    get '/login', 'user_log#login'
+    #get '/login', 'user_log#login_shib'
 
     post '/validate-login', 'user_log#validate_login'
     post '/logout', 'user_log#log_out'
     post '/check', 'user_log#check_log'
 
-    # Administrative endpoints
+    # Administrative endpoints.
     post '/check-admin', 'admin#check_admin'
     post '/check-admin-token', 'admin#check_admin_token'
 
