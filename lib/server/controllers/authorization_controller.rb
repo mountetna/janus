633180610206cf77ea220d29a8d2227f8cd63802
Saurabh Asthana <saurabh.asthana@ucsf.edu>
2020-10-09 11:23:07 -0700

    log unauthorized users

--- lib/server/controllers/authorization_controller.rb
+++ lib/server/controllers/authorization_controller.rb
@@ -107,7 +107,10 @@ class AuthorizationController < Janus::Controller
 
     # Get and check user. No password required.
     user = User[email: email]
-    raise Etna::Unauthorized unless user
+    unless user
+      Janus.instance.logger.warn("Unauthorized request for #{email}")
+      raise Etna::Unauthorized
+    end
 
     # Create a new token for the user.
     token = user.create_token!
