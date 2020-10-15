3afc9b78bb830b7a98f48c20bba9261d5a0aec73
Zach Collins <recursive.cookie.jar@gmail.com>
2020-10-15 13:43:40 -0700

    Flowjo ETL + Etl DSL / Script Manager (#105)
    Flowjo ETL + Etl DSL / Script Manager

--- lib/commands.rb
+++ lib/commands.rb
@@ -1,19 +1,4 @@
 class Janus
-  class Help < Etna::Command
-    usage 'List this help'
-
-    def execute
-      puts 'Commands:'
-      Janus.instance.commands.each do |name,cmd|
-        puts cmd.usage
-      end
-    end
-
-    def setup(config)
-      Janus.instance.configure(config)
-    end
-  end
-
   class Console < Etna::Command
     usage 'Open a console with a connected magma instance.'
 
