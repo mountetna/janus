384029876669eaaff46c43634d1a486863c00c83
Cole <4930129+coleshaw@users.noreply.github.com>
2022-01-11 14:49:37 -0500

    also wrap save/cancel in othes janus pages

--- lib/client/jsx/save-cancel.jsx
+++ lib/client/jsx/save-cancel.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
+import 'regenerator-runtime/runtime';
+
 import Icon from 'etna-js/components/icon';
+import useAsyncWork from 'etna-js/hooks/useAsyncWork';
 
 const SaveCancel = ({onSave, onCancel}) => {
+  const [_, safeOnSave] = useAsyncWork(onSave, {cancelWhenChange: []});
   return (
     <React.Fragment>
-      <Icon className='approve' icon='save' onClick={onSave} />
+      <Icon className='approve' icon='save' onClick={safeOnSave} />
       <Icon className='cancel' icon='ban' onClick={onCancel} />
     </React.Fragment>
   );
