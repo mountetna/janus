2fcf8621f07eca1f31366933edf88104f777033e
Cole <4930129+coleshaw@users.noreply.github.com>
2022-06-01 11:42:28 -0400

    update constraint name

--- db/migrations/012_update_permissions_role_validation.rb
+++ db/migrations/012_update_permissions_role_validation.rb
@@ -4,7 +4,7 @@ Sequel.migration do
     alter_table(:permissions) do
       drop_constraint("permissions_role_check")
       validate do
-        includes(["administrator", "editor", "viewer", "guest"], :role, name: "permission_allowable_roles")
+        includes(["administrator", "editor", "viewer", "guest"], :role, name: "permissions_role_check")
       end
     end
   end
@@ -13,7 +13,7 @@ Sequel.migration do
     extension(:constraint_validations)
     alter_table(:permissions) do
       validate do
-        drop_constraint("permission_allowable_roles")
+        drop_constraint("permissions_role_check")
         includes(["administrator", "editor", "viewer"], :role, name: "permissions_role_check")
       end
     end
